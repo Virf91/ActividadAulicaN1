c77fd75dd10f9049af3d5cc9a8d46cfc591210d1
alan-um <urquizamanzanelli.aj@gmail.com>
2024-03-22 16:35:29 -0300

    Agregamos metodo barajar y mostrar

--- ActividadAulica/src/actividadaulica/ActividadAulica.java
+++ ActividadAulica/src/actividadaulica/ActividadAulica.java
@@ -6,6 +6,10 @@ public class ActividadAulica {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        Baraja baraja1 = new Baraja();
+        
+        baraja1.barajar();
+        baraja1.mostrarCartas();
     }
     
 }
--- ActividadAulica/src/actividadaulica/Baraja.java
+++ ActividadAulica/src/actividadaulica/Baraja.java
@@ -2,14 +2,16 @@
 package actividadaulica;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Baraja {
    
     private ArrayList<Carta> cartas;
 
     public Baraja() {
-      
-      String[] palos = {"Espadas", "Bastos", "Oros", "Copas"};
+       cartas = new ArrayList();
+       
+       String[] palos = {"Espadas", "Bastos", "Oros", "Copas"};
         for (String palo : palos) {
             for (int numero = 1; numero <= 12; numero++) {
                 if (numero != 8 && numero != 9) {
@@ -21,6 +23,22 @@ public class Baraja {
     
     
     }
+    
+    public void barajar(){
+        Collections.shuffle(cartas);
+    }
+    
+    public void mostrarCartas(){
+        if(cartas.size()==0){
+            System.out.println("La baraja está vacía.");
+        }else{
+            System.out.println("La baraja es:");
+            for(Carta aux:cartas){
+                System.out.println(aux.toString());
+            }
+        }
+    }
+    
 }
     
     
