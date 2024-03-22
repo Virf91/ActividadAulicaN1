8109154144a0351bcd2191d1c4dc98f8b15853ba
Maximiliano Balestrieri <maximiliano_balestrieri@hotmail.com>
2024-03-22 16:45:25 -0300

    baraja
    cambio to string

--- ActividadAulica/src/actividadaulica/Carta.java
+++ ActividadAulica/src/actividadaulica/Carta.java
@@ -12,7 +12,7 @@ public class Carta {
 
     @Override
     public String toString() {
-        return "Carta{" + "num=" + num + ", palo=" + palo + '}';
+        return "Carta{" +  num + " de " + palo + '}';
     }
     
     
