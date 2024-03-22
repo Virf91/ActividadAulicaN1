2ba6afadaf210e9dc04c0d004b4ebc5aeaf4390c
alan-um <urquizamanzanelli.aj@gmail.com>
2024-03-22 15:49:55 -0300

    Creando Contrustor

--- ActividadAulica/src/actividadaulica/Baraja.java
+++ ActividadAulica/src/actividadaulica/Baraja.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 public class Baraja {
    
     private ArrayList<Carta> cartas;
+
+    public Baraja() {
+        String[] palos = {"Espada","Basto","Copa","Oro"};
+    }
+    
+    
     
     
     
